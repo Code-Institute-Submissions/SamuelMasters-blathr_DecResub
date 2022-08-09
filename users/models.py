1c83dda3278212f8a83ad72d83251541676fa569
SamuelMasters <samuelmastersdev@gmail.com>
2022-08-09 19:54:29 +0000

    Install and wireup django-allauth

--- users/models.py
+++ users/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 # Switch to django allauth for next commit
 
-class User(models.Model):
-    """
-    Data-model for user profiles registered on the site.
-    """
-    user_id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=20)
-    email = models.EmailField()
+# class User(models.Model):
+#     """
+#     Data-model for user profiles registered on the site.
+#     """
+#     user_id = models.IntegerField(primary_key=True)
+#     name = models.CharField(max_length=20)
+#     email = models.EmailField()
