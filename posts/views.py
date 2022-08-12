fa5863fe16ba42f50ad4edd62242ec40e1dab794
SamuelMasters <samuelmastersdev@gmail.com>
2022-08-12 09:45:25 +0000

    Add skeleton for post iteration on index.html

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-from django.views import View
+from django.views import View, generic
+from .models import Post
 
 # def home(request):
 #     return render(request, 'base.html')
 
 
-class Homepage(View):
+# class Homepage(View):
 
-    def get(self, request):
-        return render(request, 'index.html')
+#     model = Post
+#     queryset = Post.objects
+
+#     def get(self, request):
+#         return render(request, 'index.html')
+
+
+class Homepage(generic.ListView):
+
+    model = Post
+    queryset = Post.objects.all()
+    template_name = 'index.html'
