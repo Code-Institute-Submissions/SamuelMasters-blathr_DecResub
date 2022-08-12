6cb72810e45d43a154981fab3a08f73f75ff909b
SamuelMasters <samuelmastersdev@gmail.com>
2022-08-12 17:33:03 +0000

    Add django.contrib.sites to settings.py

--- blathr/settings.py
+++ blathr/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -51,8 +52,11 @@ INSTALLED_APPS = [
     'users',
 ]
 
+SITE_ID = 1
+
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
