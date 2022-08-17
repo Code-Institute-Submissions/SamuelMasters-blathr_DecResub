f46f6af0ad291e200ea7c859f6ae6e4a897e2fea
SamuelMasters <samuelmastersdev@gmail.com>
2022-08-17 19:52:26 +0000

    Install crispy forms and rework existing files to utilise

--- posts/views.py
+++ posts/views.py
@@ -4,25 +4,41 @@ from django.template import loader
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Post, Category
+from .forms import PostForm
 
 
 def home(request):
     post_list = Post.objects.all()
     category_list = Category.objects.all()
-    return render(request, 'index.html', {'post_list': post_list, 'category_list':category_list})
+    return render(request, 'index.html', {'post_list': post_list, 'category_list': category_list})
 
 
-def add(request):
-    template = loader.get_template('add.html')
-    return HttpResponse(template.render({}, request))
+# def add(request):
+#     template = loader.get_template('add.html')
+#     return HttpResponse(template.render({}, request))
+
+
+# def add_post(request):
+#     post_title = request.POST['post-title']
+#     post_content = request.POST['post-content']
+#     post_category = request.POST['post-category']
+#     post_author = request.user
+#     print(post_author)  # remove before final deploy
+#     new_post = Post(title=post_title, author=post_author, content=post_content, category='Finance')
+#     new_post.save()
+#     return HttpResponseRedirect(reverse('index'))
 
 
 def add_post(request):
-    post_title = request.POST['post-title']
-    post_content = request.POST['post-content']
-    new_post = Post(title=post_title, author='admin', content=post_content, category='Finance')
-    new_post.save()
-    return HttpResponseRedirect(reverse('index'))
+    categories = Category.objects.all()
+
+    return render(
+        request,
+        'add.html',
+        {
+            "post_form": PostForm()
+        },
+    )
 
 
 # class Homepage(generic.ListView):
