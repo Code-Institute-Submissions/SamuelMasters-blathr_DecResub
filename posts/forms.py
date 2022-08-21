6466cfc35f3ce960fed1398dd458258dd08c0dcb
SamuelMasters <samuelmastersdev@gmail.com>
2022-08-21 11:59:59 +0000

    Edit files to prevent attempting to access request object in forms.py

--- posts/forms.py
+++ posts/forms.py
@@ -1,14 +1,15 @@
 from .models import Post
 from django import forms
+from django.contrib.auth.models import User
 
 
 class PostForm(forms.ModelForm):
     title = forms.CharField(max_length=100)
     content = forms.TextInput()
 
-    # def __init__(self, author):
-    #     self.author = request.user.username
-    #     super(PostForm, self).__init__()
+    # def __init__(self, *args, **kwargs):
+    #     super(PostForm, self).__init__(*args, **kwargs)
+    #     self.fields['User'] = request.user
 
     class Meta:
         model = Post
--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from django.urls import reverse
 from .models import Post, Category
 from .forms import PostForm
+from django.contrib.auth.models import User
 
 
 
@@ -14,23 +15,30 @@ def home(request):
 
 def add_post(request, form=PostForm):
 
+    def get_form_kwargs(self):
+        kwargs = super(add_post, self).get_form_kwargs
+        kwargs['request'] = self.request
+        return kwargs
+
     if request.POST:
         new_post = form(request.POST)
-        print("Request was recognised as POST.")
+        print("DEBUG: request was recognised as POST.")
 
         if new_post.is_valid():
-            print("new_post was recognised as valid.")
+            print("DEBUG: new_post was recognised as valid.")
             new_post.author = request.user
-            print("new_post.author id set to " + str(request.user.id))  # debug
-            print("new_post.author username set to " + str(request.user.username))  # debug
-            print("new_post.author is currently: " + str(new_post.author))
+            print("DEBUG: new_post.author id set to " + str(request.user.id))  # debug
+            print("DEBUG: new_post.author username set to " + str(request.user.username))  # debug
+            print("DEBUG: new_post.author is currently: " + str(new_post.author))  # debug
+            print("DEBUG: printing new_post object...")
+            print(new_post)
             new_post.save()
         redirect(home)
 
     current_user_id = request.user.id  # debug
     current_user_username = request.user.username  # debug
-    print(current_user_id)  # debug
-    print(current_user_username)  # debug
+    print("DEBUG: current_user_id set to: " + str(current_user_id))  # debug
+    print("DEBUG: current_user_username set to: " + current_user_username)  # debug
 
     return render(
         request,
