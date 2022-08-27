07cb586888cd20279c0ce19de4a04c12bc242201
SamuelMasters <samuelmastersdev@gmail.com>
2022-08-27 19:15:40 +0000

    Add responsive sizing to summernote forms

--- blathr/settings.py
+++ blathr/settings.py
@@ -159,3 +159,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SUMMERNOTE_CONFIG = {
+
+    # You can put custom Summernote settings
+    'summernote': {
+
+        # Change editor size
+        'width': '100%',
+        'height': '480',
+
+    },
+
+}
